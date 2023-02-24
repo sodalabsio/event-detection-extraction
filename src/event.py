b27e84591a9266d4144c5d5be58a36abcb8fa943
satya <satyabratborgohain@gmail.com>
2023-02-24 13:36:19 +1100

    added support for CPU/GPU (device agnostic)

--- src/event.py
+++ src/event.py
@@ -7,16 +7,18 @@ EVENT_Q = "What type of event happened?"  # trigger questions
 # source: https://huggingface.co/veronica320/QA-for-Event-Extraction
 MODEL_PATH = 'veronica320/QA-for-Event-Extraction'
 EVENT_Q = "What type of event happened?"  # trigger questions
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+DEVICE_ID = 0 if torch.cuda.is_available() else -1
 
 
 class QAEvent:
 
-    def __init__(self, model_path, device=0):
+    def __init__(self, model_path, device, device_id):
         tokenizer = AutoTokenizer.from_pretrained(model_path)
         model = AutoModelForQuestionAnswering.from_pretrained(model_path)
-        model.to(torch.device("cuda"))  # model to GPU
+        model.to(device)  # model to CPU/GPU depending on availability
         self.pipeline = pipeline(
-            "question-answering", model=model, tokenizer=tokenizer, device=device)
+            "question-answering", model=model, tokenizer=tokenizer, device=device_id)
 
     def predict(self, question, context):
         try:
@@ -32,9 +34,9 @@ def run_event_extraction(df):
     events = []
     rows = df.title.tolist()
     # NB: make sure there's enough space in HDD
-    qa_event = QAEvent(model_path=MODEL_PATH)
+    qa_event = QAEvent(model_path=MODEL_PATH,
+                       device=DEVICE, device_id=DEVICE_ID)
     for row_title in tqdm(rows):
-        row_event = []
         try:
             answer = qa_event.predict(question=EVENT_Q, context=row_title)
             if answer:
@@ -42,10 +44,10 @@ def run_event_extraction(df):
                               confidence=answer.get('score')))
 
             else:
-                events.append(dict(event=None, score=None))
+                events.append(dict(event=None, confidence=None))
 
         except Exception as e:
             print(e)
-            events.append(dict(event=None, score=None))
+            events.append(dict(event=None, confidence=None))
 
     return events
